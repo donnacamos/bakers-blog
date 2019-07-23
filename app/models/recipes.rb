722605e190c1f08500a36157b9e6cb98603cc5e5
Donna Amos <donnacamos.developer@gmail.com>
2019-07-23 14:45:30 +0000

    update models

--- app/models/recipes.rb
+++ app/models/recipes.rb
@@ -1,4 +1,5 @@
 class Recipes < ActiveRecord::Base 
+  has_many :ingredients 
   has_many :shopping_lists
   belongs_to :user 
 end 
--- app/models/shopping_lists.rb
+++ app/models/shopping_lists.rb
@@ -1,4 +1,5 @@
 class ShoppingLists < ActiveRecord::Base 
+  has_many :ingredients 
   belongs_to :user 
   belongs_to :recipes
 end 
